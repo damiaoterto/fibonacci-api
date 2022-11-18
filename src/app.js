5f266afcf7d75f70024e178a00182923b31aeaf2
Francisco Damião Terto <hi@damiaojunior.me>
2022-11-17 21:58:01 -0300

    create fibonacci  end-point

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const fibonacci = require('fibonacci');
 
 const app = express();
+
+app.use(express.json());
+
+app.get('/fibonacci', (req, res) => {
+  const result = fibonacci.iterate(req.query.iterations || 3000);
+  return res.json(result);
+});
 
 module.exports = { app };
